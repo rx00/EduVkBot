93ffc7ebe8226e072b5d9dca21d089de01606b15
Roman Bykov <it.tec@me.com>
2018-03-31 05:43:09 +0500

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -2,7 +2,7 @@ from message import Message
 
 
 def main_handler(message: Message):
-    if message.body.startswith("Мур"):
+    if message.body.startswith("Мур"): # в message.body хранится текст полученного сообщения
         return "Mяу"  # если функция вернет str, то бот ответит простым сообщением
     else:
         return 46  # если функция вернет int, то бот ответит пользователю id стикера
